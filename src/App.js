c34dd432ccc3af0a1e01128449640db44fa76635
EddieCarrion.github.io <ecar25L@gmail.com>
2023-05-05 16:50:21 -0700

    progress

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import Footer from "./components/Footer"
 import Header from "./components/Header"
 import CatIndex from "./pages/CatIndex"
@@ -10,15 +10,54 @@ import NotFound from "./pages/NotFound"
 import './App.css';
 import { Routes, Route } from "react-router-dom"
 
-import mockCats from "./mockCats"
+
+// import mockCats from "./mockCats"
 
 const App =() => {
-  const [cats, setCats] = useState(mockCats)
-  console.log(cats)
+  const [cats, setCats] = useState([])
+  useEffect(()=>{
+    readCats()
+  },[])
 
-  const createCat = (cat) => {
-    console.log(cat)
+  const readCats = () => {
+    fetch("http://localhost:3000/cats")
+    .then((response) => response.json())
+    .then(payload => {
+      setCats(payload)
+    })
+    
+    .catch((error => console.log(error)))
   }
+
+  const createCat = (cats) => {
+    fetch("http://localhost:3000/cats",{
+      body: JSON.stringify(cats),
+      headers: {
+        "Content-Type": "application/json"
+      },
+      method: "POST"
+    })
+    .then((response) => response.json())
+    .then(() => readCats())
+    .catch((errors) => console.log("Cat create errors:", errors))
+  }
+  const updateCat = (cats, id) => {
+    fetch(`http://localhost:3000/cats/${id}`, {
+      
+      body: JSON.stringify(cats),
+     
+      headers: {
+        "Content-Type": "application/json"
+      },
+      
+      method: "PATCH"
+    })
+      .then((response) => response.json())
+      .then((payload) => readCats ())
+      .catch((errors) => console.log("Cat update errors:", errors))
+  }
+
+
   return (
    <>
       <Header />
@@ -27,7 +66,7 @@ const App =() => {
         <Route CatIndex path="/catindex" element={<CatIndex cats={cats} />} />
         <Route path="/catshow/:id" element={<CatShow cats={cats} />} />        
         <Route CatNew path="/catnew" element={<CatNew createCat={createCat} />} /> 
-        <Route CatEdit path="/catEdit" element={<CatEdit/>} />
+        <Route path="/catEdit/:id" element={<CatEdit cats={cats} updateCat={updateCat}/>} />
         <Route NotFound path="*" element={<NotFound/>} />
       </Routes>
       <Footer />
--- src/pages/CatEdit.js
+++ src/pages/CatEdit.js
@@ -1,11 +1,81 @@
-import React from "react"
+import { useState } from "react"
+import { useNavigate, useParams } from "react-router-dom"
+import { Form, FormGroup, Input, Label, Button } from "reactstrap"
 
-const CatEdit = () => {
-    return(
-        <>
-            <h3>CatEdit</h3>
-        </>
-    )
+const CatEdit = ({ cats, updateCat }) => {
+  const { id } = useParams()
+  let currentCat = cats?.find((cat) => cat.id === +id)
+
+  const [editCat, setEditCat] = useState({
+    name: currentCat.name,
+    age: currentCat.age,
+    enjoys: currentCat.enjoys,
+    image: currentCat.image,
+  })
+
+  const handleChange = (e) => {
+    setEditCat({ ...editCat, [e.target.name]: e.target.value })
+  }
+
+  const navigate = useNavigate()
+  const handleSubmit = (e) => {
+    updateCat(editCat, currentCat.id)
+    navigate(`/catshow/${id}`)
+  }
+
+  return (
+    <div>
+      <Form>
+        <FormGroup>
+          <Label for="cat-name">Name</Label>
+          <Input
+            id="cat-name"
+            name="name"
+            placeholder="Enter cat's name"
+            type="text"
+            onChange={handleChange}
+            value={editCat.name}
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="cat-age">Age</Label>
+          <Input
+            id="cat-age"
+            name="age"
+            placeholder="Enter cat's age"
+            type="text"
+            onChange={handleChange}
+            value={editCat.age}
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="cat-enjoys">enjoys</Label>
+          <Input
+            id="cat-enjoys"
+            name="enjoys"
+            placeholder="Enter cat's enjoys"
+            type="text"
+            onChange={handleChange}
+            value={editCat.enjoys}
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="cat-image">image</Label>
+          <Input
+            id="cat-image"
+            name="image"
+            placeholder="Enter a picture of the cat"
+            type="text"
+            onChange={handleChange}
+            value={editCat.image}
+          />
+        </FormGroup>
+        <Button color="primary" onClick={handleSubmit}>
+          Submit Updated Cat
+        </Button>
+      </Form>
+    </div>
+  )
 }
 
 export default CatEdit
